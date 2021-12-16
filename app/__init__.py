3912c04984450f7afe9f1e9477482e70c7cfcf7c
siddharthaadapa <65726806+siddharthaadapa@users.noreply.github.com>
2021-12-16 21:02:32 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,8 @@ import pymysql
 pymysql.install_as_MySQLdb()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "sarvesh"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://sidhu:root@localhost/keycap'
+app.config['SECRET_KEY'] = "sidhu"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://root:sidhu@localhost/keycap'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
